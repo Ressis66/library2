3cb8c492928416ad347b98cfa66fd0aa38fadac1
Ressis66 <vaganov.aleksandr.88@mail.ru>
2022-11-03 15:18:38 +0500

    Tenth try

--- src/main/java/ru/otus/library2/controllers/CommentsController.java
+++ src/main/java/ru/otus/library2/controllers/CommentsController.java
@@ -2,9 +2,12 @@ package ru.otus.library2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -29,5 +32,10 @@ public class CommentsController {
     return new ResponseEntity<>(comments, HttpStatus.OK);
   }
 
+  @PostMapping(value = "/comments", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Mono<Comment>> addCommentToBook(@RequestBody Comment comment) {
+    Mono<Comment> comment1 = commentRepository.insert(comment);
+    return new ResponseEntity<>(comment1, HttpStatus.CREATED);
+  }
 
 }
